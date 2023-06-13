6392bd4a9f00f507983ac179fb59b47242593e7e
Paweł Hajdo <pawel.hajdo001@gmail.com>
2023-06-13 20:23:57 +0200

    created start screen controller

--- app/BtattleshipsApp/src/main/java/com/battleships/battleshipsapp/HelloApplication.java
+++ app/BtattleshipsApp/src/main/java/com/battleships/battleshipsapp/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("start_screen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 800);
+        stage.setTitle("Battleships");
         stage.setScene(scene);
         stage.show();
     }
---
+++ app/BtattleshipsApp/src/main/java/com/battleships/battleshipsapp/StartScreenController.java
@@ -0,0 +1,17 @@
+package com.battleships.battleshipsapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class StartScreenController {
+
+    @FXML
+    protected void onLoginButtonClick() {
+
+    }
+
+    @FXML
+    private void onPlayAsGuestButtonClick(){
+
+    }
+}
